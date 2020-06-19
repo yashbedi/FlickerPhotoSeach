c38a2d46e5ebe86e3b2f89f634e452f326de8b6b
yashbedi <yashbeerbedi@gmail.com>
2020-06-19 22:53:42 +0530

    subclassing collectionViewCell

---
+++ FlickerPhotoSearch/FlickerPhotoCell.swift
@@ -0,0 +1,13 @@
+//
+//  FlickerPhotoCell.swift
+//  FlickerPhotoSearch
+//
+//  Created by Yash Bedi on 19/06/20.
+//  Copyright © 2020 Yash Bedi. All rights reserved.
+//
+
+import UIKit
+
+final class FlickerPhotoCell: UICollectionViewCell {
+    s
+}
